60246d068ce6f65500c12ee4a7a689c40c114d3b
MiningTcup <125408675+MiningTcup@users.noreply.github.com>
2023-02-18 09:57:08 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Numerics;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -7,24 +7,28 @@ namespace MultipleRoblox
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Welcome to Roblox Multi-Instance!\n");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write("You must close all Roblox instances before continuing or the hack will not work.\nPress any key when you have closed all Roblox instances: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("--- INSTRUCTIONS ---\n 1. Close all Roblox instances\n 2. Press any key to activate Multi-Instance");
             System.Console.ReadKey();
             Console.WriteLine("\n");
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Activating ROBLOX_singletonMutex...");
             new Mutex(true, "ROBLOX_singletonMutex");
             Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Made by MiningTcup, some credit to MainDabRblx\n\n--- NOTES ---\nYou may now open as many Roblox instances as you like, ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("using seperate accounts");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(". If you close this window, all Roblox instances except for one will close. Make sure that you closed all Roblox instances before activating this.\n");
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Activation Complete\n");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("--NOTES--");
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.Write("You may now open as many Roblox instances as you like, using sperate accounts.\nIf you close this window, all but 1 Roblox instance will close.\nBy MiningTcup\nCredits to MainDabRblx (Github). ");
+            Console.WriteLine("Roblox Multi-Instance is now activated!\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Please do not close this window.");
             Console.ForegroundColor = ConsoleColor.White;
             Thread.Sleep(-1);
         }
